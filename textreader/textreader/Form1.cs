6d22792cb91b788739c4fd25a0fcf958a966d093
Will Purman <willpurman@gmail.com>
2020-04-15 11:19:44 -0700

    initial push
    push text reader app

---
+++ textreader/textreader/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace textreader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int CountCharacters()
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader("c:\\data.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+
+            return count;
+        }
+
+        private async void btnProcessFile_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CountCharacters);
+            task.Start();
+            lblcount.Text = "Processing File...";
+            int count = await task;
+            lblcount.Text = count.ToString() + " characters!";
+        }
+    }
+}
